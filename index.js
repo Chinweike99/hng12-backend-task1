64b2c16b635d2a00e6c3d02a37a74d7544e3b81e
Chinweike99 <innocentakwolu1@gmail.com>
2025-02-04 18:47:15 +0100

    checked for negative integers

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import axios from 'axios';
-import { primeNumber, perfectNumber, isArmstrong, digitSum } from "./herpers.js"; // Import functions from helpers.js
 
 
 
@@ -11,13 +10,54 @@ const port = process.env.PORT || 3000;
 // Enable CORS
 app.use(cors());
 
+// Function to check if num is a prime number
+const primeNumber = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+// Function to check if number is  perfect.
+const perfectNumber = (num) => {
+  let sum = 0;
+  for (let i = 1; i < num; i++) {
+    if (num % i === 0) sum += i;
+  }
+  return sum === num;
+};
+
+/**
+ * isArmstrong - function to check if a number.
+ * armstrong: is a number that is equal to the sum of its own digits, each raised to the power of the number of digits.
+ */
+const isArmstrong = (num) => {
+  const digits = String(num).split("");
+  const sum = digits.reduce(
+    (acc, digit) => acc + Math.pow(Number(digit), digits.length),
+    0
+  );
+  return sum === num;
+};
+
+// Function to add didgits
+const digitSum = (num) => {
+  return String(num)
+    .split("")
+    .reduce((acc, digit) => acc + Number(digit), 0);
+};
+
+
 
 // API endpoint
 app.get("/api/classify-number", async (req, res) => {
     const num = parseInt(req.query.number);
+    // const {num} = req.query
+    
   
     // Validate input
-    if (isNaN(num)) {
+    if (isNaN(num) || num < 0) {
       return res.status(400).json({
         number: req.query.number,
         error: true,
